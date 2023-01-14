80453db8f5756ca66c38a4ef51037173c0f30909
Eli Joe <elijoe365@pm.me>
2023-01-14 18:54:11 +0000

    expand exploration of dataset, add script for concatenation of w-nut 2021 train and dev set

--- definitions.py
+++ definitions.py
@@ -1,7 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(ROOT_DIR, "data/external")
+DATA_PATH = os.path.join(ROOT_DIR, "data")
 
 if __name__ == "__main__":
     print(DATA_PATH)
