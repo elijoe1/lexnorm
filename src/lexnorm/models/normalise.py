7a9a8eb494214f2ac2e081f55b205d54a4ab7cd1
Eli Joe <elijoe365@pm.me>
2023-02-11 00:21:44 +0000

    Implement train and predict functions for random forest model, as well as initial trained model (rf.joblib)
    Implement functions to normalise from predictions, and prep dataframes from candidate generation for training and testing.

--- src/lexnorm/models/normalise.py
+++ src/lexnorm/models/normalise.py
@@ -1,5 +1,37 @@
-# TODO remember always normalise 'rt' to 'retweet' if followed by @mention
-# hard coded normalisation of 'rt' if followed by @mention following notebook 1.0 and 2015 annotation guideline 3.
-# we can do this as 'rt' is a domain specific entity and normalisation is fairly deterministic (when in middle
-# of tweet and not followed by @mention) and when normalised, always to 'retweet'
-# if 0 < i < len(raw_tweet) - 1 and raw_tweet[i + 1][0] != "@":
+import pandas as pd
+
+from lexnorm.models.filtering import is_eligible
+
+
+def prep_train(annotated_data):
+    train = pd.read_csv(annotated_data, index_col=0)
+    train_X = train.fillna(0).drop(
+        ["correct", "gold", "process", "tweet", "tok"], axis=1
+    )
+    train_y = train.fillna(0)["correct"]
+    return train_X, train_y
+
+
+def prep_test(unannotated_data):
+    train = pd.read_csv(unannotated_data, index_col=0)
+    test_X = train.fillna(0).drop(["process", "tweet", "tok"], axis=1)
+    return test_X
+
+
+def normalise(raw, pred_toks):
+    pred_tokens_iter = iter(pred_toks)
+    pred_tweets = []
+    for tweet in raw:
+        pred_tweet = []
+        for i, tok in enumerate(tweet):
+            if is_eligible(tok):
+                pred_tweet.append(next(pred_tokens_iter))
+            elif tok == "rt" and 0 < i < len(tweet) - 1 and tweet[i + 1][0] != "@":
+                # hard coded normalisation of 'rt' if followed by @mention following notebook 1.0 and 2015 annotation guideline 3.
+                # we can do this as 'rt' is a domain specific entity and normalisation is fairly deterministic (when in middle
+                # of tweet and not followed by @mention) and when normalised, always to 'retweet'
+                pred_tweet.append("retweet")
+            else:
+                pred_tweet.append(tok)
+        pred_tweets.append(pred_tweet)
+    return pred_tweets
--- src/lexnorm/models/random_forest.py
+++ src/lexnorm/models/random_forest.py
@@ -1,20 +1,45 @@
 import os
 
 import pandas as pd
-from joblib import dump
+from joblib import dump, load
 from sklearn.ensemble import RandomForestClassifier
 
+from lexnorm.data.normEval import loadNormData, evaluate
 from lexnorm.definitions import DATA_PATH
+from lexnorm.models.normalise import prep_train, prep_test, normalise
+
+
+def train(train_X, train_y, output_file):
+    rf_clf = RandomForestClassifier(
+        n_jobs=-1,
+        random_state=42,
+        verbose=1,
+        class_weight="balanced",
+        oob_score=True,
+    )
+    rf_clf.fit(train_X, train_y)
+    dump(rf_clf, output_file)
+
+
+def predict(model, features, data):
+    probs = model.predict_proba(features)
+    data["probs"] = probs[:, 1]
+    preds = data.sort_values("probs", ascending=False).drop_duplicates(
+        ["process", "tweet", "tok"]
+    )
+    pred_tokens = preds.sort_values(["process", "tweet", "tok"]).index.tolist()
+    return pred_tokens
 
 
 if __name__ == "__main__":
-    train = pd.read_csv(os.path.join(DATA_PATH, "hpc/train_annotated.txt"), index_col=0)
-    train_X = train.fillna(0).drop(
-        ["correct", "gold", "process", "tweet", "tok"], axis=1
-    )
-    train_y = train.fillna(0)["correct"]
-    rf_clf = RandomForestClassifier(
-        n_jobs=-1, random_state=42, verbose=1, class_weight="balanced"
-    )
-    rf_clf.fit(train_X, train_y)
-    dump(rf_clf, os.path.join(DATA_PATH, "../models/rf.joblib"))
+    # train(
+    #     *prep_train(os.path.join(DATA_PATH, "hpc/train_annotated.txt")),
+    #     os.path.join(DATA_PATH, "../models/rf.joblib"),
+    # )
+    raw, norm = loadNormData(os.path.join(DATA_PATH, "raw/dev.norm"))
+    clf = load(os.path.join(DATA_PATH, "../models/rf.joblib"))
+    dev_X = prep_test(os.path.join(DATA_PATH, "hpc/dev_unannotated.txt"))
+    dev = pd.read_csv(os.path.join(DATA_PATH, "hpc/dev_unannotated.txt"), index_col=0)
+    pred_tokens = predict(clf, dev_X, dev)
+    predictions = normalise(raw, pred_tokens)
+    evaluate(raw, norm, predictions)
